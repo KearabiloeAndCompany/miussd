0dfbfbf36ee0fdb503e898c11c2d2a975667a0e3
kea <kea@crowdcoin.co.za>
2017-11-21 14:53:03 +0200

    Updated static settings

--- BookingPlatform/urls.py
+++ BookingPlatform/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from BookingUssd.views import *
+from django.views import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT}),
     url(r'^ussd/$' , view=ussdView, name='ussd'),
 ]
