27030b70928a67e73f372286da08438b7e2a90c1
kea <kea@crowdcoin.co.za>
2018-01-30 05:47:33 +0200

    Fix migration field type error

--- BookingUssd/migrations/0021_auto_20180129_0821.py
+++ BookingUssd/migrations/0021_auto_20180129_0821.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL('ALTER TABLE church_booking_submission_message ALTER tag_id TYPE varchar(32);'),
+        migrations.RunSQL('ALTER TABLE BookingUssd_church_booking_booking_update ALTER tag_id TYPE varchar(32);'),
         migrations.AlterField(
             model_name='church',
             name='booking_submission_message',
